8e314a2c667f7c22b6846dd83e860d2e491aa084
Murilo Moreira <murilojosemoreira@hotmail.com>
2021-04-24 03:55:18 -0300

    Configurando cors do backend para liberar acesso a api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 require('./database');
 
 const app = express();
 
+/* configurando cors para liberar acesso a api */
+app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(routes);
+
 
 app.listen(3333);
